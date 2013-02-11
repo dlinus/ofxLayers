398bede0987ffb97e931939ea2453de11db55fb9
rezaali <syed.reza.ali@gmail.com>
2013-02-10 17:50:10 -0800

    added src

---
+++ src/ofxLayer.h
@@ -0,0 +1,306 @@
+/********************************************************************************** 
+ 
+ Copyright (C) 2012 Syed Reza Ali (www.syedrezaali.com)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ **********************************************************************************/
+
+#ifndef OFXLAYER
+#define OFXLAYER
+
+#include "ofEvents.h"
+#include "ofMain.h"
+
+using namespace std; 
+
+class ofxLayer; 
+
+class ofxSharedAppData; 
+
+class ofxLayerEventArgs : public ofEventArgs {
+public:
+    ofxLayerEventArgs( string layerName , ofxLayer *sender)
+    {
+        this->layerName = layerName; 
+        this->sender = sender; 
+    }
+    ofxLayerEventArgs( string layerName , ofxLayer *sender, string message)
+    {
+        this->layerName = layerName; 
+        this->sender = sender; 
+        this->message = message;         
+    }
+
+    string layerName;
+    string message; 
+    ofxLayer *sender; 
+};
+
+class ofxLayer
+{
+public:
+	ofxLayer()
+	{
+		active = false;
+        bSetup = false; 
+	}
+
+	virtual ~ofxLayer() 
+	{
+	}
+    
+    virtual void setup() {} 
+    virtual void update() {}
+    virtual void draw() {}
+    virtual void exit() {}
+    
+    virtual string getLayerName() {return layerName;}
+
+    bool isActive() { return active; } 
+    
+    bool isSetup() { return bSetup; } 
+    
+    void setSetup(bool _bSetup)
+    {
+        bSetup = _bSetup;
+    }
+
+    virtual void activate() 
+    {
+        active = true; 
+    }                              
+	
+    virtual void deactivate() 
+    {
+        active = false;     
+    } 	
+    
+    void activateLayer(string name)
+    {
+        ofxLayerEventArgs args = ofxLayerEventArgs(name, this); 
+        ofNotifyEvent(activateLayerEvent, args, this);
+    }
+
+    void deactivateLayer(string name)
+    {
+        ofxLayerEventArgs args = ofxLayerEventArgs(name, this); 
+        ofNotifyEvent(deactivateLayerEvent, args, this);        
+    }
+    
+    void switchLayer(string name)
+    {
+        ofxLayerEventArgs args = ofxLayerEventArgs(name, this);         
+        ofNotifyEvent(switchLayerEvent, args, this);
+    }
+
+    void switchLayer(string name, string message)
+    {
+        ofxLayerEventArgs args = ofxLayerEventArgs(name, this, message);         
+        ofNotifyEvent(switchLayerEvent, args, this);
+    }
+    
+    ofEvent<ofxLayerEventArgs> switchLayerEvent; 
+    ofEvent<ofxLayerEventArgs> activateLayerEvent;    
+    ofEvent<ofxLayerEventArgs> deactivateLayerEvent;    
+    
+    virtual void touchDown(ofTouchEventArgs& touch) {}
+    virtual void touchMoved(ofTouchEventArgs& touch) {}
+    virtual void touchUp(ofTouchEventArgs& touch) {}
+    virtual void touchDoubleTap(ofTouchEventArgs& touch) {}
+    virtual void touchCancelled(ofTouchEventArgs& touch) {}
+    
+#ifndef TARGET_OPENGLES
+    
+    virtual void mouseReleased(ofMouseEventArgs& data) {}
+    virtual void mousePressed(ofMouseEventArgs& data) {}
+    virtual void mouseMoved(ofMouseEventArgs& data) {}
+    virtual void mouseDragged(ofMouseEventArgs& data) {}
+    
+    virtual void keyPressed(int key) {}
+    virtual void keyReleased(int key) {}
+    virtual void windowResized(int w, int h) {}
+
+#endif
+    
+    void setSharedAppData(ofxSharedAppData* sharedAppData) { this->sharedAppData = sharedAppData; }
+    
+#ifdef TARGET_ANDROID
+    virtual void savePressed(string title, string tags){};
+    virtual void imageSelected(string imageURL){};
+    virtual void pause(){};
+	virtual void stop(){};
+	virtual void resume(){};
+	virtual void reloadTextures(){}
+
+    virtual void onKeyDown(int keyCode){}    
+	virtual void onKeyUp(int keyCode){}
+    
+	virtual bool backPressed(){ return false; }
+	virtual void menuPressed(){}
+    
+	virtual bool menuItemSelected(string menu_id_str){ return false; }    
+	virtual bool menuItemChecked(string menu_id_str, bool checked){ return false; }
+    
+	virtual void okPressed(){};
+	virtual void cancelPressed(){};
+	virtual void gotFile(string url, string filename){};
+	virtual void urlResponse(ofHttpResponse & response) {};
+    
+#endif
+    
+#ifdef TARGET_OF_IPHONE
+
+    virtual void lostFocus() {}
+    virtual void gotFocus() {}
+    virtual void gotMemoryWarning() {}
+    virtual void deviceOrientationChanged(int newOrientation) {}
+    
+#endif
+    
+protected:
+    ofxSharedAppData* sharedAppData;
+	string layerName; 
+    bool active;    //means that the layer should be updating and drawing
+    bool bSetup; 
+};
+
+#endif
+
+
+
+/* Template to copy
+ 
+ #ifndef OFX_
+ #define OFX_
+ 
+ #include "ofxLayer.h"
+ 
+ class className : public ofxLayer
+ {
+ public:
+	 //--------------------------------------------------------------
+	 className()
+	 {
+		layerName = "className";
+	 }
+	 //--------------------------------------------------------------
+	 ~className() 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void setup() 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void update() 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void draw() 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void exit() 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void activate() 
+	 {
+		active = true; 
+	 }                              
+	 
+	 void deactivate() 
+	 {
+		active = false;     
+	 } 	
+	 //--------------------------------------------------------------
+	 void touchDown(ofTouchEventArgs& touch) 
+	 {
+	 
+	 }
+	 //-------------------------------------------------------------- 
+	 void touchMoved(ofTouchEventArgs& touch) 
+	 {
+	 
+	 }
+	 //-------------------------------------------------------------- 
+	 void touchUp(ofTouchEventArgs& touch) 
+	 {
+	 
+	 }
+	 //-------------------------------------------------------------- 
+	 void touchDoubleTap(ofTouchEventArgs& touch) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void touchCancelled(ofTouchEventArgs& touch) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void mouseReleased(ofMouseEventArgs& data) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void mousePressed(ofMouseEventArgs& data) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void mouseMoved(ofMouseEventArgs& data) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void mouseDragged(ofMouseEventArgs& data) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void keyPressed(int key) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void keyReleased(int key) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+	 void windowResized(int w, int h) 
+	 {
+	 
+	 }
+	 //--------------------------------------------------------------
+ 
+ protected:		//	ofxSharedAppData* sharedAppData;	string layerName; 	bool active;    Reference
+
+ };
+ 
+ #endif
+ 
+*/
---
+++ src/ofxLayerManager.h
@@ -0,0 +1,527 @@
+/********************************************************************************** 
+ 
+ Copyright (C) 2012 Syed Reza Ali (www.syedrezaali.com)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ **********************************************************************************/
+
+#ifndef OFXLAYERMANAGER
+#define OFXLAYERMANAGER
+
+#include "ofxLayer.h"
+#include <map>
+
+class ofxLayerManager
+{
+public: 
+    
+    typedef map<string, ofxLayer*>::iterator layerIt;    
+    
+	ofxLayerManager()
+    {
+        enableAppEventCallbacks();
+#ifdef TARGET_OPENGLES
+        enableTouchCallbacks();
+#else
+        enableMouseEventCallbacks();
+        enableKeyEventCallbacks();
+        enableWindowEventCallbacks(); 
+#endif 
+    }
+	
+	~ofxLayerManager()
+	{
+        //exit function handles the dynamic memory...gets called by openframeworks before quiting
+	}
+    
+    void enable()
+    {
+        enableAppEventCallbacks();        
+    }
+    
+    void disable()
+    {
+        disableAppEventCallbacks();
+    }
+
+    
+    //App Data 
+    void setSharedAppData(ofxSharedAppData* sharedAppData) { this->sharedAppData = sharedAppData; }
+    
+    //Layer Adding & Setup
+
+    void addLayer(ofxLayer* newlayer)
+    {
+        newlayer->setSharedAppData(sharedAppData);
+//        newlayer->setup();
+        layers[newlayer->getLayerName()] = newlayer;
+        ofAddListener(newlayer->switchLayerEvent, this, &ofxLayerManager::onSwitchLayer);
+        ofAddListener(newlayer->activateLayerEvent, this, &ofxLayerManager::onActivateLayer);
+        ofAddListener(newlayer->deactivateLayerEvent, this, &ofxLayerManager::onDeactivateLayer);        		
+    }
+    
+    void onActivateLayer(ofxLayerEventArgs &args)
+    {
+        layerIt it = layers.find(args.layerName);
+        if (it != layers.end())
+        {
+        	ofxLayer *l = (ofxLayer *) it->second;
+            if(!l->isSetup())
+            {
+                l->setup();
+                l->setSetup(true);
+            }
+        	l->activate();
+        }
+    }
+
+    void onDeactivateLayer(ofxLayerEventArgs &args)
+    {
+        layerIt it = layers.find(args.layerName);
+        if (it != layers.end())
+        {
+            ofxLayer *l = (ofxLayer *) it->second; 
+            l->deactivate(); 
+        }        
+    }
+
+    void onSwitchLayer(ofxLayerEventArgs &args)
+    {        
+        layerIt it = layers.find(args.layerName);
+        if (it != layers.end()) 
+        {            
+            args.sender->deactivate(); 
+            ofxLayer *l = (ofxLayer *) it->second;
+            if(!l->isSetup())
+            {
+                l->setup();
+                l->setSetup(true);
+            }            
+            l->activate();
+        }          
+    }
+    
+	void activateLayer(ofxLayer *_layer)
+	{
+        layerIt it = layers.find(_layer->getLayerName());
+        if (it != layers.end())
+        {
+            ofxLayer *l = (ofxLayer *) it->second;
+            if(!l->isSetup())
+            {
+                l->setup();
+                l->setSetup(true);
+            }            
+            l->activate(); 
+        }		
+	}
+	
+	void switchLayer(ofxLayer *_layer)
+	{
+		layerIt it = layers.find(_layer->getLayerName());        
+		if (it != layers.end())
+        {
+            for (layer= layers.begin(); layer != layers.end(); layer++)
+            {
+                ofxLayer *last = (ofxLayer *) layer->second;                
+                if(last->isSetup())
+                {
+                    last->deactivate(); 
+                }
+            }
+            ofxLayer *l = (ofxLayer *) it->second;
+            if(!l->isSetup())
+            {
+                l->setup();
+                l->setSetup(true);
+            }            
+            l->activate();             
+        }
+    }
+    
+	void switchLayer(string name)
+	{
+		layerIt it = layers.find(name);        
+		if (it != layers.end())
+        {
+            for (layer= layers.begin(); layer != layers.end(); layer++)
+            {
+                ofxLayer *last = (ofxLayer *) layer->second;                
+                if(last->isSetup())
+                {
+                    last->deactivate(); 
+                }
+            }
+            ofxLayer *l = (ofxLayer *) it->second;
+            if(!l->isSetup())
+            {
+                l->setup();
+                l->setSetup(true);
+            }            
+            l->activate();             
+        }
+    }    
+	
+    ofxSharedAppData* getSharedAppData()
+    {
+        return sharedAppData;
+    }
+    
+    map<string, ofxLayer*> getLayers() const
+    {
+        return layers;
+    }
+    
+    vector<string> getLayerNames()
+    {
+        vector<string> layerNames; 
+        for (layer= layers.begin(); layer != layers.end(); layer++)
+        {
+            ofxLayer *last = (ofxLayer *) layer->second;   
+            layerNames.push_back(last->getLayerName());
+        }
+        return layerNames; 
+    }
+    
+    //App Callbacks
+    void enableAppEventCallbacks()
+    {
+        ofAddListener(ofEvents().update, this, &ofxLayerManager::onUpdate);
+        ofAddListener(ofEvents().draw, this, &ofxLayerManager::onDraw);
+        ofAddListener(ofEvents().exit, this, &ofxLayerManager::onExit);
+    }
+
+    void disableAppEventCallbacks()
+    {
+        ofRemoveListener(ofEvents().update, this, &ofxLayerManager::onUpdate);
+        ofRemoveListener(ofEvents().draw, this, &ofxLayerManager::onDraw);
+        ofRemoveListener(ofEvents().exit, this, &ofxLayerManager::onExit);
+    }
+
+    void onUpdate(ofEventArgs &data) 
+    {
+        update();
+    }
+    void onDraw(ofEventArgs &data) 
+    { 
+        draw();
+    } 
+    void onExit(ofEventArgs &data) 
+    { 
+        exit();
+    } 
+    
+    void update()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isActive()) { layer->second->update(); }
+        }
+    }
+    
+    void draw()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isActive()) { layer->second->draw(); }
+        }
+    }
+    
+    void exit()
+    {
+        cout << "Exiting LayerManager" << endl;
+
+        disable();
+        for (layer = layers.begin(); layer != layers.end(); layer++ )
+        {
+            ofxLayer *l = layer->second;
+            if(l->isSetup())
+            {
+                l->exit();
+            }
+            delete l;
+        }
+		layers.clear();
+    }
+    
+#ifdef TARGET_OPENGLES
+    //Touch Callbacks
+    void enableTouchCallbacks()
+    {
+        ofAddListener(ofEvents().touchUp, this, &ofxLayerManager::onTouchUp);
+        ofAddListener(ofEvents().touchDown, this, &ofxLayerManager::onTouchDown);
+        ofAddListener(ofEvents().touchMoved, this, &ofxLayerManager::onTouchMoved);
+        ofAddListener(ofEvents().touchCancelled, this, &ofxLayerManager::onTouchCancelled);
+        ofAddListener(ofEvents().touchDoubleTap, this, &ofxLayerManager::onTouchDoubleTap);
+    }
+    
+    void onTouchUp(ofTouchEventArgs &data) 
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->touchUp(data); }        
+    }
+    
+    void onTouchDown(ofTouchEventArgs &data)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->touchDown(data); }
+    }
+    
+    void onTouchMoved(ofTouchEventArgs &data) 
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->touchMoved(data); }        
+    }
+    void onTouchCancelled(ofTouchEventArgs &data)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->touchCancelled(data); }
+    }
+    void onTouchDoubleTap(ofTouchEventArgs &data)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->touchDoubleTap(data); }
+    }
+#else
+    //Keyboard Callbacks
+    void enableKeyEventCallbacks()
+    {
+        ofAddListener(ofEvents().keyPressed, this, &ofxLayerManager::onKeyPressed);
+        ofAddListener(ofEvents().keyReleased, this, &ofxLayerManager::onKeyReleased);
+    }
+    
+    void onKeyPressed(ofKeyEventArgs& data)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->keyPressed(data.key); }
+        
+    }
+    
+    void onKeyReleased(ofKeyEventArgs& data)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->keyReleased(data.key); }
+    }    
+    //Mouse Callbacks
+    void enableMouseEventCallbacks()
+    {
+        ofAddListener(ofEvents().mouseReleased, this, &ofxLayerManager::onMouseReleased);
+        ofAddListener(ofEvents().mousePressed, this, &ofxLayerManager::onMousePressed);
+        ofAddListener(ofEvents().mouseMoved, this, &ofxLayerManager::onMouseMoved);
+        ofAddListener(ofEvents().mouseDragged, this, &ofxLayerManager::onMouseDragged);
+    }
+    
+    void onMouseReleased(ofMouseEventArgs& data) 
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->mouseReleased(data); }         
+    }
+    
+    void onMousePressed(ofMouseEventArgs& data) 
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->mousePressed(data); }         
+    }
+    
+    void onMouseMoved(ofMouseEventArgs& data) 
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->mouseMoved(data); }         
+    }
+    
+    void onMouseDragged(ofMouseEventArgs& data) 
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->mouseDragged(data); }             
+    }
+    
+    //Window Resize Callback
+    void enableWindowEventCallbacks()
+    {
+        ofAddListener(ofEvents().windowResized, this, &ofxLayerManager::onWindowResized);        
+    }
+    
+    void onWindowResized(ofResizeEventArgs& data) 
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isSetup())
+            {   
+                layer->second->windowResized(data.width, data.height); 
+            }
+        }
+    }       
+#endif      
+    
+#ifdef TARGET_ANDROID
+    
+    void urlResponse(ofHttpResponse & response)
+    {
+    	for (layer= layers.begin(); layer != layers.end(); layer++ )
+			if(layer->second->isActive()) { layer->second->urlResponse(response); }
+    }
+
+    void imageSelected(string imageURL)
+    {
+    	for (layer= layers.begin(); layer != layers.end(); layer++ )
+    		if(layer->second->isActive()) { layer->second->imageSelected(imageURL); }
+    }
+
+    void gotFile(string url, string filename)
+	{
+		for (layer= layers.begin(); layer != layers.end(); layer++ )
+			if(layer->second->isActive()) { layer->second->gotFile(url, filename); }
+	}
+
+    void savePressed(string title, string tags)
+	{
+		for (layer= layers.begin(); layer != layers.end(); layer++ )
+			if(layer->second->isActive()) { layer->second->savePressed(title, tags); }
+	}
+
+    void pause()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++)
+            layer->second->pause();         
+    }
+    
+	void stop()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++)
+            layer->second->stop();         
+    }
+    
+    void resume()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++)
+            layer->second->resume();         
+    }
+    
+    void reloadTextures()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++)
+            layer->second->reloadTextures();             
+    }
+    
+    void onKeyDown(int keyCode)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->onKeyDown(keyCode); }             
+    }    
+    
+	void onKeyUp(int keyCode)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->onKeyUp(keyCode); }             
+    }
+    
+	bool backPressed()
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { return layer->second->backPressed(); }             
+    }
+
+	void menuPressed()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { layer->second->menuPressed(); }             
+    }
+    
+	bool menuItemSelected(string menu_id_str)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { return layer->second->menuItemSelected(menu_id_str); }             
+    }    
+	
+    bool menuItemChecked(string menu_id_str, bool checked)
+    { 
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { return layer->second->menuItemChecked(menu_id_str, checked); }             
+    }
+    
+	void okPressed()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { return layer->second->okPressed(); }                 
+    }
+	
+    void cancelPressed()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+            if(layer->second->isActive()) { return layer->second->cancelPressed(); }                 
+    }        
+
+#endif      
+    
+    
+#ifdef TARGET_OF_IPHONE
+    
+    void lostFocus()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isSetup())
+            {
+                layer->second->lostFocus();
+            }
+        }    
+    }
+    void gotFocus()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isSetup())
+            {
+                layer->second->gotFocus();
+            }
+        }
+    }
+    
+    void gotMemoryWarning()
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isSetup())
+            {
+                layer->second->gotMemoryWarning();
+            }
+        }
+    }
+    
+    void deviceOrientationChanged(int newOrientation)
+    {
+        for (layer= layers.begin(); layer != layers.end(); layer++ )
+        {
+            if(layer->second->isSetup())
+            {
+                layer->second->deviceOrientationChanged(newOrientation);
+            }
+        }
+    }
+    
+#endif
+    
+private:
+    layerIt layer; 
+    map<string, ofxLayer*> layers;    
+    ofxSharedAppData *sharedAppData; 
+};
+
+#endif 
